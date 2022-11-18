85eb81c75cca2c35aff2827601e84ce492cd1410
Sebastian Alcock <78321301+SebastianAlcock@users.noreply.github.com>
2022-11-17 20:31:19 -0500

    Update model.py

--- model.py
+++ model.py
@@ -18,61 +18,61 @@ from keras.models import Model
 from keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, concatenate, Conv2DTranspose, BatchNormalization, Dropout, Lambda
 from keras import backend as K
 
-def multi_unet_model(n_classes=4, IMG_HEIGHT=256, IMG_WIDTH=256, IMG_CHANNELS=1):
+def multi_unet_model(n_classes=4, IMG_HEIGHT=256, IMG_WIDTH=256, IMG_CHANNELS=1, activation_type='relu'):
 #Build the model
     inputs = Input((IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS))
     #s = Lambda(lambda x: x / 255)(inputs)   #No need for this if we normalize our inputs beforehand
     s = inputs
 
     #Contraction path
-    c1 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(s)
+    c1 = Conv2D(16, (3, 3), activation=activation_type, kernel_initializer='he_normal', padding='same')(s)
     c1 = Dropout(0.2)(c1)  # Original 0.1
-    c1 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c1)
+    c1 = Conv2D(16, (3, 3), activation=activation_type, kernel_initializer='he_normal', padding='same')(c1)
     p1 = MaxPooling2D((2, 2))(c1)
     
-    c2 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p1)
+    c2 = Conv2D(32, (3, 3), activation=activation_type, kernel_initializer='he_normal', padding='same')(p1)
     c2 = Dropout(0.2)(c2)  # Original 0.1
-    c2 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c2)
+    c2 = Conv2D(32, (3, 3), activation=activation_type, kernel_initializer='he_normal', padding='same')(c2)
     p2 = MaxPooling2D((2, 2))(c2)
      
-    c3 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p2)
+    c3 = Conv2D(64, (3, 3), activation=activation_type, kernel_initializer='he_normal', padding='same')(p2)
     c3 = Dropout(0.2)(c3)
-    c3 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c3)
+    c3 = Conv2D(64, (3, 3), activation=activation_type, kernel_initializer='he_normal', padding='same')(c3)
     p3 = MaxPooling2D((2, 2))(c3)
      
-    c4 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p3)
+    c4 = Conv2D(128, (3, 3), activation=activation_type, kernel_initializer='he_normal', padding='same')(p3)
     c4 = Dropout(0.2)(c4)
-    c4 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c4)
+    c4 = Conv2D(128, (3, 3), activation=activation_type, kernel_initializer='he_normal', padding='same')(c4)
     p4 = MaxPooling2D(pool_size=(2, 2))(c4)
      
-    c5 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p4)
+    c5 = Conv2D(256, (3, 3), activation=activation_type, kernel_initializer='he_normal', padding='same')(p4)
     c5 = Dropout(0.3)(c5)
-    c5 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c5)
+    c5 = Conv2D(256, (3, 3), activation=activation_type, kernel_initializer='he_normal', padding='same')(c5)
     
     #Expansive path 
     u6 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(c5)
     u6 = concatenate([u6, c4])
-    c6 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u6)
+    c6 = Conv2D(128, (3, 3), activation=activation_type, kernel_initializer='he_normal', padding='same')(u6)
     c6 = Dropout(0.2)(c6)
-    c6 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c6)
+    c6 = Conv2D(128, (3, 3), activation=activation_type, kernel_initializer='he_normal', padding='same')(c6)
      
     u7 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(c6)
     u7 = concatenate([u7, c3])
-    c7 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u7)
+    c7 = Conv2D(64, (3, 3), activation=activation_type, kernel_initializer='he_normal', padding='same')(u7)
     c7 = Dropout(0.2)(c7)
-    c7 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c7)
+    c7 = Conv2D(64, (3, 3), activation=activation_type, kernel_initializer='he_normal', padding='same')(c7)
      
     u8 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(c7)
     u8 = concatenate([u8, c2])
-    c8 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u8)
+    c8 = Conv2D(32, (3, 3), activation=activation_type, kernel_initializer='he_normal', padding='same')(u8)
     c8 = Dropout(0.2)(c8)  # Original 0.1
-    c8 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c8)
+    c8 = Conv2D(32, (3, 3), activation=activation_type, kernel_initializer='he_normal', padding='same')(c8)
      
     u9 = Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same')(c8)
     u9 = concatenate([u9, c1], axis=3)
-    c9 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u9)
+    c9 = Conv2D(16, (3, 3), activation=activation_type, kernel_initializer='he_normal', padding='same')(u9)
     c9 = Dropout(0.2)(c9)  # Original 0.1
-    c9 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c9)
+    c9 = Conv2D(16, (3, 3), activation=activation_type, kernel_initializer='he_normal', padding='same')(c9)
      
     outputs = Conv2D(n_classes, (1, 1), activation='softmax')(c9)
      
@@ -227,8 +227,13 @@ X_train, X_test, y_train, y_test = train_test_split(image_dataset, labels_cat, t
 #Parameters for model
 
 params = {
-    'learning_rate': 0.001
+    'learning_rate': 0.001,
+    'activation_type': 'relu',
+    'batch_size': 16
 }
+
+optimized_params = nni.get_next_parameter()
+params.update(optimized_params)
 
 weights = [0.1666, 0.1666, 0.1666, 0.1666, 0.1666, 0.1666]
 dice_loss = sm.losses.DiceLoss(class_weights=weights) 
@@ -239,7 +244,7 @@ IMG_HEIGHT = X_train.shape[1]
 IMG_WIDTH  = X_train.shape[2]
 IMG_CHANNELS = X_train.shape[3]
 
-model = multi_unet_model(n_classes=n_classes, IMG_HEIGHT=IMG_HEIGHT, IMG_WIDTH=IMG_WIDTH, IMG_CHANNELS=IMG_CHANNELS)
+model = multi_unet_model(n_classes=n_classes, IMG_HEIGHT=IMG_HEIGHT, IMG_WIDTH=IMG_WIDTH, IMG_CHANNELS=IMG_CHANNELS, activation_type=params['activation_type'])
 
 adam = tf.keras.optimizers.Adam(learning_rate=params['learning_rate'])
 
@@ -249,7 +254,7 @@ callback = tf.keras.callbacks.LambdaCallback(
     on_epoch_end = lambda epoch, logs: nni.report_intermediate_result(logs['accuracy'])
 )
 
-model.fit(X_train, y_train, batch_size=16, verbose=1, epochs=10, validation_data=(X_test, y_test), callbacks=[callback])
-accuracy = model.evaluate(X_test, y_test, verbose=1)
+model.fit(X_train, y_train, batch_size=params['batch_size'], epochs=10, validation_data=(X_test, y_test), callbacks=[callback])
+accuracy = model.evaluate(X_test, y_test)
 
 nni.report_final_result(accuracy)
